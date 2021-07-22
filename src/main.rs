e078b8db37e3ab3f32dfa9e9957af65cbf9e9aea
cht <3237126351@qq.com>
2021-07-22 12:21:50 +0800

    support running

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ use std::{
     fs::{self, File},
     io::prelude::*,
     path::Path,
+    process::Command,
 };
 //use std::cell::RefCell;
 #[derive(Copy, Clone)]
@@ -60,6 +61,211 @@ thread_local!(
         local: 0,
     });
 );
+fn run(name: &Urls) {
+    let mut json = String::new();
+    let temp = name.port.clone();
+    let length = temp.len();
+    let port: String = (&temp[1..length - 1]).to_string();
+    let temp2 = name.aid.clone();
+    let length2 = temp2.len();
+    let aid: String = (&temp2[1..length2 - 1]).to_string();
+    let output = format!(
+        "{{
+    \"inbounds\":[{{
+        \"port\":8889,
+        \"listen\":\"127.0.0.1\",
+        \"protocol\":\"http\",
+        \"settings\":{{
+            \"udp\": true
+        }}
+    }}],
+    \"outbounds\":[{{
+        \"protocol\":{},
+        \"sendThrough\": \"0.0.0.0\",
+        \"settings\":{{
+            \"vnext\": [{{
+                \"address\": {},
+                \"port\":{},
+                \"users\":[{{
+                    \"alterId\": {},
+                    \"id\":{}
+                }}]
+            }}]
+        }},
+        \"streamSettings\":{{
+            \"dsSettings\": {{
+                \"path\": {}
+            }},
+            \"httpSettings\":{{
+                \"host\": [
+                ],
+                \"path\":{}
+            }},
+            \"kcpSettings\": {{
+                \"congestion\": false,
+                \"downlinkCapacity\":20,
+                \"header\": {{
+                    \"type\": \"none\"
+                }},
+                \"mtu\": 1350,
+                \"readBufferSize\": 1,
+                \"tti\": 20,
+                \"uplinkCapacity\": 5,
+                \"writeBufferSize\": 1
+            }},
+            \"network\": {},
+            \"quicSettings\":{{
+                \"header\": {{
+                    \"type\":\"none\"
+                }},
+                \"key\": \"\",
+                \"security\":\"\"
+            }},
+            \"security\":\"none\",
+            \"sockopt\":{{
+                \"mark\": 255,
+                \"tcpFastOpen\": false,
+                \"tproxy\": \"off\"
+            }},
+            \"tcpSettings\": {{
+                \"header\": {{
+                    \"request\" :{{
+                        \"headers\":{{
+                        }},
+                        \"method\": \"GET\",
+                        \"path\":[
+                        ],
+                        \"version\":\"1.1\"
+                    }},
+                    \"type\": \"none\"
+                }}
+            }},
+            \"tlsSettings\": {{
+                \"allowInsecure\": true,
+                \"allowInsecureCiphers\": true,
+                \"alpn\":[
+                ],
+                \"certificates\":[
+                ],
+                \"disableSessionResumption\":true,
+                \"disableSystemRoot\":true,
+                \"serveName\": \"\"
+            }},
+            \"wsSettings\" :{{
+                \"headers\" :{{
+                }},
+                \"path\":{}
+            }},
+            \"xtlsSettings\":{{
+                \"allowInsecure\":true,
+                \"allowInsecureCiphers\":true,
+                \"alpn\":[
+                ],
+                \"certificates\":[
+                ],
+                \"disableSessionResumption\": false,
+                \"disableSystemRoot\": true,
+                \"serveName\":\"\"
+            }},
+            \"tag\":\"outBound_PROXY\"
+        }}
+    }},
+    {{
+        \"protocol\":\"freedom\",
+        \"tag\": \"direct\",
+        \"settings\":{{}}
+    }}],
+    \"routing\": {{
+        \"domainStrategy\": \"IPOnDemand\",
+        \"rules\":[{{
+            \"type\":\"field\",
+            \"ip\":[\"geoip:private\"],
+            \"outboundTag\": \"direct\"
+        }}]
+    }}
+}}",
+        name.func, name.add, port, aid, name.id, name.path, name.path, name.net, name.path
+    );
+    json.push_str(output.as_str());
+    let home = env::var("HOME").unwrap();
+    let location = home + "/.config/gv2ray/running.json";
+    let path2 = Path::new(location.as_str());
+    //let display = path.display();
+    //let path2 = Path::new("storage.json");
+    let display2 = path2.display();
+    let mut file2 = match File::create(&path2) {
+        Err(why) => panic!("couldn't create {}: {}", display2, why.to_string()),
+        Ok(file2) => file2,
+    };
+
+    // 将 `LOREM_IPSUM` 字符串写进 `file`，返回 `io::Result<()>`
+    //match file2.write_all(json.as_bytes()) {
+    //    Err(why) => {
+    //        panic!("couldn't write to {}: {}", display2, why.to_string())
+    //    }
+    //    Ok(_) => {}
+    //}
+    if let Err(why) = file2.write_all(json.as_bytes()) {
+        panic!("couldn't write to {}: {}", display2, why.to_string())
+    }
+    Command::new("pkill")
+        .arg("v2ray")
+        .output()
+        .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+
+    let home2 = env::var("HOME").unwrap();
+    let location = home2.clone() + "/.config/gv2ray/v2core.json";
+    let path = Path::new(location.as_str());
+    //let display = path.display();
+    let mut file = match File::open(&path) {
+        // `io::Error` 的 `description` 方法返回一个描述错误的字符串。
+        Err(_) => {
+            let path2 = Path::new(location.as_str());
+            let display2 = path2.display();
+            let mut file2 = match File::create(&path2) {
+                Err(why) => panic!("couldn't create {}: {}", display2, why.to_string()),
+                Ok(file2) => file2,
+            };
+            let mut storge2: String = String::new();
+            storge2.push_str("{\n\"v2core\":\"/usr/bin/v2ray\"\n}");
+            // 将 `LOREM_IPSUM` 字符串写进 `file`，返回 `io::Result<()>`
+            if let Err(why) = file2.write_all(storge2.as_bytes()) {
+                panic!("couldn't write to {}: {}", display2, why.to_string())
+            }
+            let path3 = Path::new(location.as_str());
+            File::open(&path3).unwrap()
+        }
+        Ok(file) => file,
+    };
+    let mut ss = String::new();
+    let mut content: String = String::new();
+    match file.read_to_string(&mut ss) {
+        Err(_) => {}
+        Ok(_) => {
+            let v: Value = serde_json::from_str(ss.as_str()).unwrap();
+            let temp = v["v2core"].to_string();
+            let length = temp.len();
+            content = (&temp[1..length - 1]).to_string();
+        }
+    }
+    Command::new(content)
+        .arg("-config")
+        .arg(home2.clone() + "/.config/gv2ray/running.json")
+        .spawn()
+        .expect("failed");
+
+    //Command::new("nohup")
+    //    .arg(content)
+    //    .arg("-config")
+    //    .arg(home2.clone() + "/.config/tv2ray/running.json")
+    //    .arg(">")
+    //    .arg(home2 + "/.config/tv2ray/test.log")
+    //    .arg("2>&1")
+    //    .arg("&")
+    //    .spawn()
+    //    .expect("failed");
+}
+
 fn create_storage_before() {
     let home = env::var("HOME").unwrap();
     fs::create_dir_all(home + "/.config/gv2ray").unwrap();
@@ -390,6 +596,12 @@ fn build_ui(application: &gtk::Application) {
                             is_running: -1,
                             local: temp,
                         };
+                        Command::new("pkill")
+                            .arg("v2ray")
+                            .output()
+                            .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+
+
                     } else {
                         s.set_label("stop");
                         //println!("{},{}",locall.is_running,locall.local);
@@ -397,6 +609,11 @@ fn build_ui(application: &gtk::Application) {
                             is_running: temp,
                             local: temp,
                         };
+                        GLOBALURL.with(|globalurl|{
+                            if let Some(ref url) = *globalurl.borrow(){
+                                run(&url[temp as usize]);
+                            }
+                        });
                     }
                 });
             });
@@ -414,6 +631,11 @@ fn build_ui(application: &gtk::Application) {
                         local: path.indices()[0],
                     };
                     ui.running_button.set_label("stop");
+                });
+                GLOBALURL.with(|globalurl|{
+                    if let Some(ref url) = *globalurl.borrow(){
+                        run(&url[path.indices()[0] as usize]);
+                    }
                 });
             }
         });
