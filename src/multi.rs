e7d151c2328f36e599fdcd3c34d6052ba41b2bf4
cht <3237126351@qq.com>
2021-08-07 20:24:08 +0800

    支持多订阅

--- src/multi.rs
+++ src/multi.rs
@@ -1,5 +1,9 @@
 use crate::tool::{get_v2ray, write_json};
 use gtk::prelude::*;
+use std::cell::RefCell;
+thread_local! {
+    static GLOBAL: RefCell<Option<gtk::Box>> = RefCell::new(None);
+}
 pub fn create_sub_window(
     application: &gtk::Application,
     title: &str,
@@ -21,21 +25,62 @@ pub fn create_sub_window(
     // urls设置界面
     {
         let boxs = gtk::Box::new(gtk::Orientation::Vertical, 10);
+        let boxs2 = gtk::Box::new(gtk::Orientation::Vertical, 10);
         let urls_input = gtk::Entry::new();
         let button_box = gtk::ButtonBox::new(gtk::Orientation::Horizontal);
         button_box.set_layout(gtk::ButtonBoxStyle::End);
         let button = gtk::Button::with_label("Input");
+        let button2 = gtk::Button::with_label("Upload");
         button_box.pack_start(&button, false, false, 0);
+        button_box.pack_start(&button2, false, false, 0);
+        boxs.pack_start(&boxs2, true, false, 0);
         boxs.pack_start(&urls_input, true, false, 0);
         boxs.pack_start(&button_box, false, false, 0);
-        button.connect_clicked(glib::clone!(@weak model =>move |_|{
-                //model.clear();
-                let input: String = urls_input.text().to_string();
-                let temp : Vec<String> = vec![input];
-                func(&model, temp);
+        GLOBAL.with(move |global|{
+            *global.borrow_mut() =Some(boxs2);
+        });
+        button.connect_clicked(move |_|{
+            let urls = urls_input.text().to_string();
+            GLOBAL.with(move |global|{
+                if let Some(ref bos) = *global.borrow(){
+                    create_url(bos, urls);
+                }
+            });
+            //create_url(&boxs2, urls);
+        });
+        button2.connect_clicked(glib::clone!(@weak model =>move |_|{
+            let mut output : Vec<String> = vec![];
+            GLOBAL.with(move |global|{
+                if let Some(ref boxs2) = *global.borrow(){
+                    for index in boxs2.children() {
+                        // 通过gtk的子类的事件，获取到entry的控件，从而获取内容
+                        let temp : String = index.downcast_ref::<gtk::Box>().unwrap().children()[0].downcast_ref::<gtk::Entry>().unwrap().text().to_string();
+                        output.push(temp);
+                    }
+                }
+                //println!("{:?}",output);
+                func(&model,output);
+            })
         }));
         create_tab(&notebook, "urls", boxs.upcast());
     }
+    //{
+    //    let boxs = gtk::Box::new(gtk::Orientation::Vertical, 10);
+    //    let urls_input = gtk::Entry::new();
+    //    let button_box = gtk::ButtonBox::new(gtk::Orientation::Horizontal);
+    //    button_box.set_layout(gtk::ButtonBoxStyle::End);
+    //    let button = gtk::Button::with_label("Input");
+    //    button_box.pack_start(&button, false, false, 0);
+    //    boxs.pack_start(&urls_input, true, false, 0);
+    //    boxs.pack_start(&button_box, false, false, 0);
+    //    //button.connect_clicked(glib::clone!(@weak model =>move |_|{
+    //    //        //model.clear();
+    //    //        let input: String = urls_input.text().to_string();
+    //    //        let temp : Vec<String> = vec![input];
+    //    //        func(&model, temp);
+    //    //}));
+    //    create_tab(&notebook, "urls", boxs.upcast());
+    //}
     {
         let boxs = gtk::Box::new(gtk::Orientation::Vertical, 10);
         let urls_input = gtk::Entry::new();
@@ -59,23 +104,7 @@ pub fn create_sub_window(
         });
         create_tab(&notebook, "v2ray", boxs.upcast());
     }
-    {
-        let boxs = gtk::Box::new(gtk::Orientation::Vertical, 10);
-        let boxs2 = gtk::Box::new(gtk::Orientation::Vertical, 10);
-        let urls_input = gtk::Entry::new();
-        let button_box = gtk::ButtonBox::new(gtk::Orientation::Horizontal);
-        button_box.set_layout(gtk::ButtonBoxStyle::End);
-        let button = gtk::Button::with_label("Input");
-        button_box.pack_start(&button, false, false, 0);
-        boxs.pack_start(&boxs2, true, false, 0);
-        boxs.pack_start(&urls_input, true, false, 0);
-        boxs.pack_start(&button_box, false, false, 0);
-        button.connect_clicked(move |_|{
-            let urls = urls_input.text().to_string();
-            create_url(&boxs2, urls);
-        });
-        create_tab(&notebook, "test", boxs.upcast());
-    }
+
     window.add(&notebook);
     window.connect_delete_event(
         // drop的信号
@@ -100,6 +129,12 @@ fn create_url(boxs: &gtk::Box,urls: String) {
     button.connect_clicked(glib::clone!(@weak urls_input,@weak boxs => move |_|{
         boxs.remove(&url_box);
     }));
+    //println!("{:?}",boxs.children());
+    //if boxs.children().len() > 1{
+    //    //println!("{:?}",boxs.children()[0]);
+    //    //这个可以通过gtk自带的反射获取内容
+    //    println!("{}",(boxs.children()[0].downcast_ref::<gtk::Box>().unwrap().children()[0]).downcast_ref::<gtk::Entry>().unwrap().text());
+    //}
     //显示所有
     boxs.show_all();
 }
